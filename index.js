56c6bc78aabb58b8e6b9493c21f84a1502277077
Joe Minichino <joe.minichino@gmail.com>
2014-09-27 22:43:51 +0100

    first import

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 function EventEmitter() {}
 EventEmitter.prototype.events = {};
 EventEmitter.prototype.on = function (eventName, listener) {
-	var event = this.events[eventName];
-	if (!event) {
-		event = this.events[eventName] = [];
-	}
-	return event.push(listener) - 1;
+  var event = this.events[eventName];
+  if (!event) {
+    event = this.events[eventName] = [];
+  }
+  return event.push(listener) - 1;
 };
 EventEmitter.prototype.emit = function (eventName) {
-	if (this.events[eventName]) {
-		this.events[eventName].forEach(function (listener) {
-			listener.call(null);
-		});
-	} else {
-		throw new Error('No event ' + eventName + ' defined');
-	}
+  if (this.events[eventName]) {
+    this.events[eventName].forEach(function (listener) {
+      listener.call(null);
+    });
+  } else {
+    throw new Error('No event ' + eventName + ' defined');
+  }
 };
 EventEmitter.prototype.remove = function (eventName, index) {
-	if (this.events[eventName]) {
-		this.events[eventName].splice(index, 1);
-	}
+  if (this.events[eventName]) {
+    this.events[eventName].splice(index, 1);
+  }
 };
 
 function Publisher() {}
 Publisher.prototype.topics = {};
 Publisher.prototype.register = function (topic, obj, func) {
-	if (!this.topics[topic]) {
-		this.topics[topic] = [];
-	}
-	this.topics[topic].push({
-		target: obj,
-		callback: func
-	});
+  if (!this.topics[topic]) {
+    this.topics[topic] = [];
+  }
+  this.topics[topic].push({
+    target: obj,
+    callback: func
+  });
 };
 Publisher.prototype.publish = function (topic, argsArray) {
-	if (this.topics[topic]) {
-		this.topics[topic].forEach(function (handler) {
-			handler.callback.apply(handler.target, argsArray || []);
-		});
-	}
+  if (this.topics[topic]) {
+    this.topics[topic].forEach(function (handler) {
+      handler.callback.apply(handler.target, argsArray || []);
+    });
+  }
 };
 
 var pub = new Publisher();
 var o = {
-	name: 'joe'
+  name: 'joe'
 };
 pub.register('test', o, function (newName) {
-	this.name = newName;
+  this.name = newName;
 });
 pub.publish('test', ['jack']);
 console.log(o);
